2ef9fbfa65ee66d5eb1690927ac8043a62678224
Laszlo Paillat <laszlo.paillat@gmail.com>
2019-09-25 00:32:46 +0200

    fixed Warning NU5048 for futur release

--- Release.bat
+++ Release.bat
@@ -4,13 +4,13 @@ DEL /q package
 dotnet clean source\DefaultEcs\DefaultEcs.csproj -c Release
 dotnet clean source\DefaultEcs.Test\DefaultEcs.Test.csproj -c Release
 
-dotnet test source\DefaultEcs.Test\DefaultEcs.Test.csproj -c Release -r ..\..\package\ -l trx
+dotnet test source\DefaultEcs.Test\DefaultEcs.Test.csproj -c Release -r package -l trx
 
 IF %ERRORLEVEL% GTR 0 GOTO :end
 
 dotnet clean source\DefaultEcs\DefaultEcs.csproj -c Release
-dotnet clean source\DefaultEcs\DefaultEcs.Package.csproj -c Release
+dotnet clean source\DefaultEcs\DefaultEcs.Release.csproj -c Release
 
-dotnet pack source\DefaultEcs\DefaultEcs.Package.csproj -c Release -o ..\..\package\
+dotnet pack source\DefaultEcs\DefaultEcs.Release.csproj -c Release -o package
 
 :end
