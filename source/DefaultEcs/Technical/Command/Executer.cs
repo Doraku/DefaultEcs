c665df430a0aa40d8f06d3afb2af8d4ffad2f948
Laszlo Paillat <laszlo.paillat@gmail.com>
2021-01-15 23:26:37 +0100

    small cleanup

--- source/DefaultEcs/Technical/Command/Executer.cs
+++ source/DefaultEcs/Technical/Command/Executer.cs
@@ -26,55 +26,55 @@ namespace DefaultEcs.Technical.Command
                             break;
 
                         case CommandType.Enable:
-                            (*(Entity*)(memoryP + (*(EntityOffsetCommand*)commands).EntityOffset)).Enable();
+                            ((Entity*)(memoryP + ((EntityOffsetCommand*)commands)->EntityOffset))->Enable();
                             commandSize = sizeof(EntityOffsetCommand);
                             break;
 
                         case CommandType.Disable:
-                            (*(Entity*)(memoryP + (*(EntityOffsetCommand*)commands).EntityOffset)).Disable();
+                            ((Entity*)(memoryP + ((EntityOffsetCommand*)commands)->EntityOffset))->Disable();
                             commandSize = sizeof(EntityOffsetCommand);
                             break;
 
                         case CommandType.EnableT:
                             EntityOffsetComponentCommand* componentCommand = (EntityOffsetComponentCommand*)commands;
-                            ComponentCommands.GetCommand((*componentCommand).ComponentIndex).Enable(*(Entity*)(memoryP + (*componentCommand).EntityOffset));
+                            ComponentCommands.GetCommand(componentCommand->ComponentIndex).Enable(*(Entity*)(memoryP + componentCommand->EntityOffset));
                             commandSize = sizeof(EntityOffsetComponentCommand);
                             break;
 
                         case CommandType.DisableT:
                             componentCommand = (EntityOffsetComponentCommand*)commands;
-                            ComponentCommands.GetCommand((*componentCommand).ComponentIndex).Disable(*(Entity*)(memoryP + (*componentCommand).EntityOffset));
+                            ComponentCommands.GetCommand(componentCommand->ComponentIndex).Disable(*(Entity*)(memoryP + componentCommand->EntityOffset));
                             commandSize = sizeof(EntityOffsetComponentCommand);
                             break;
 
                         case CommandType.Set:
                             componentCommand = (EntityOffsetComponentCommand*)commands;
                             commandSize = sizeof(EntityOffsetComponentCommand);
-                            commandSize += ComponentCommands.GetCommand((*componentCommand).ComponentIndex).Set(*(Entity*)(memoryP + (*componentCommand).EntityOffset), objects, commands + sizeof(EntityOffsetComponentCommand));
+                            commandSize += ComponentCommands.GetCommand(componentCommand->ComponentIndex).Set(*(Entity*)(memoryP + componentCommand->EntityOffset), objects, commands + sizeof(EntityOffsetComponentCommand));
                             break;
 
                         case CommandType.SetSameAs:
                             EntityReferenceOffsetComponentCommand* entityReferenceComponentCommand = (EntityReferenceOffsetComponentCommand*)commands;
-                            ComponentCommands.GetCommand((*entityReferenceComponentCommand).ComponentIndex).SetSameAs(
-                                *(Entity*)(memoryP + (*entityReferenceComponentCommand).EntityOffset),
-                                *(Entity*)(memoryP + (*entityReferenceComponentCommand).ReferenceOffset));
+                            ComponentCommands.GetCommand(entityReferenceComponentCommand->ComponentIndex).SetSameAs(
+                                *(Entity*)(memoryP + entityReferenceComponentCommand->EntityOffset),
+                                *(Entity*)(memoryP + entityReferenceComponentCommand->ReferenceOffset));
                             commandSize = sizeof(EntityReferenceOffsetComponentCommand);
                             break;
 
                         case CommandType.Remove:
                             componentCommand = (EntityOffsetComponentCommand*)commands;
-                            ComponentCommands.GetCommand((*componentCommand).ComponentIndex).Remove(*(Entity*)(memoryP + (*componentCommand).EntityOffset));
+                            ComponentCommands.GetCommand(componentCommand->ComponentIndex).Remove(*(Entity*)(memoryP + componentCommand->EntityOffset));
                             commandSize = sizeof(EntityOffsetComponentCommand);
                             break;
 
                         case CommandType.NotifyChanged:
                             componentCommand = (EntityOffsetComponentCommand*)commands;
-                            ComponentCommands.GetCommand((*componentCommand).ComponentIndex).NotifyChanged(*(Entity*)(memoryP + (*componentCommand).EntityOffset));
+                            ComponentCommands.GetCommand(componentCommand->ComponentIndex).NotifyChanged(*(Entity*)(memoryP + componentCommand->EntityOffset));
                             commandSize = sizeof(EntityOffsetComponentCommand);
                             break;
 
                         case CommandType.Dispose:
-                            (*(Entity*)(memoryP + (*(EntityOffsetCommand*)commands).EntityOffset)).Dispose();
+                            ((Entity*)(memoryP + ((EntityOffsetCommand*)commands)->EntityOffset))->Dispose();
                             commandSize = sizeof(EntityOffsetCommand);
                             break;
                     }
