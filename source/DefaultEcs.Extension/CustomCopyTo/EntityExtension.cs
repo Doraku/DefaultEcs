24b1f92cc693ed19fb12baa4eba95c9dfd4b49ce
Laszlo Paillat <laszlo.paillat@gmail.com>
2021-05-16 17:51:26 +0200

    added CustomCopyTo example to DefaultEcs.Extension

---
+++ source/DefaultEcs.Extension/CustomCopyTo/EntityExtension.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using DefaultEcs.Serialization;
+
+namespace DefaultEcs.CustomCopyTo
+{
+    public class ComponentCloner
+    {
+        private sealed class ComponentReader : IComponentReader
+        {
+            private readonly ComponentCloner _cloner;
+
+            public ComponentReader(ComponentCloner cloner)
+            {
+                _cloner = cloner;
+            }
+
+            public void OnRead<T>(in T component, in Entity componentOwner) => _cloner.OnComponent(component, _cloner._from.IsEnabled<T>());
+        }
+
+        private readonly ComponentReader _reader;
+
+        private Entity _from;
+        private Entity _to;
+
+        public ComponentCloner()
+        {
+            _reader = new ComponentReader(this);
+        }
+
+        public void Clone(in Entity from, in Entity to)
+        {
+            _from = from;
+            _to = to;
+
+            from.ReadAllComponents(_reader);
+        }
+
+        protected void Set<T>(in T component, bool isEnabled)
+        {
+            _to.Set(component);
+            if (!isEnabled)
+            {
+                _to.Disable<T>();
+            }
+        }
+
+        protected virtual void OnComponent<T>(in T component, bool isEnabled) => Set(component, isEnabled);
+    }
+
+    public static class EntityExtension
+    {
+        public static Entity CopyTo(in this Entity entity, World world, ComponentCloner cloner)
+        {
+            if (!entity.IsAlive) throw new InvalidOperationException("Entity is not alive");
+            if (cloner is null) throw new ArgumentNullException(nameof(cloner));
+
+            Entity copy = world.CreateEntity();
+
+            if (!entity.IsEnabled())
+            {
+                copy.Disable();
+            }
+
+            try
+            {
+                cloner.Clone(entity, copy);
+            }
+            catch
+            {
+                copy.Dispose();
+
+                throw;
+            }
+
+            return copy;
+        }
+    }
+}
